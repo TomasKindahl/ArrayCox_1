2c995cd09d90f5dec18698efa54f6cafa0b50761
Tomas Kindahl <tomas.kindahl@molk.com>
2021-11-22 08:24:38 +0100

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Numerics;
+using System.Text;
+
+using ds;
+
+namespace ArrayCox_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Random randomizer = new Random();
+
+            // Stopwatches for array:
+            Stopwatch arrayAppend = new Stopwatch();
+            Stopwatch arraySet = new Stopwatch();
+            Stopwatch arrayGet = new Stopwatch();
+            Stopwatch arrayInsert = new Stopwatch();
+            Stopwatch arrayDelete = new Stopwatch();
+
+            // Stopwatches for single linked lists:
+            Stopwatch sllAppend = new Stopwatch();
+            Stopwatch sllSet = new Stopwatch();
+            Stopwatch sllGet = new Stopwatch();
+            Stopwatch sllInsert = new Stopwatch();
+            Stopwatch sllDelete = new Stopwatch();
+
+            Array<int> arr = new Array<int>(10);
+            arr.Print();
+            arr.Append(new int[] { 2, 3, 5, 7, 11 });
+            arr.Print();
+            arr.Set(2, 12);
+            arr.Print();
+            Console.WriteLine($"arr.Get(2) = {arr.Get(2)}");
+            arr.InsertAt(2, 33);
+            arr.Print();
+            arr.DeleteAt(3);
+            arr.Print();
+
+            SLList<int> sll = new SLList<int>(10);
+            sll.Print();
+            sll.Append(new int[] { 2, 3, 5, 7, 11 });
+            sll.Print();
+            sll.Set(2, 12);
+            sll.Print();
+            Console.WriteLine($"sll.Get(2) = {sll.Get(2)}");
+            sll.InsertAt(2, 33);
+            sll.Print();
+            sll.DeleteAt(3);
+            sll.Print();
+
+            for(int size = 10_000; size <= 100_000/*100_000*/; size += 10_000)
+            {
+                // Mätning av append:
+                Array<int> A = new Array<int>(size);
+                arrayAppend.Reset();
+                arrayAppend.Start();
+                for (int i = 0; i < size; i++)
+                    A.Append(randomizer.Next());
+                arrayAppend.Stop();
+                Console.WriteLine($"Appending Array<int> {size} times took {arrayAppend.Elapsed.TotalSeconds} seconds");
+
+                SLList<int> S = new SLList<int>(size);
+                sllAppend.Reset();
+                sllAppend.Start();
+                for (int i = 0; i < size; i++)
+                    S.Append(randomizer.Next());
+                sllAppend.Stop();
+                Console.WriteLine($"Appending SLList<int> {size} times took {sllAppend.Elapsed.TotalSeconds} seconds");
+
+                // Mätning av Set (typ size/10 Set-ningar):
+
+                // Mätning av Get (typ size/10 Get-ningar):
+
+                // Mätning av InsertAt (typ size/10 InsertAt-ningar):
+
+                // Mätning av DeleteAt (typ size/10 DeleteAt-ningar):
+            }
+        }
+    }
+}
---
+++ ds.cs
@@ -0,0 +1,151 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ds
+{
+    // Array
+    public class Array<T>
+    {
+        private T[] arr;
+        int currentSize;
+        public Array(int mSize)
+        {
+            arr = new T[mSize];
+            currentSize = 0;
+        }
+        public void Append(T val)
+        {
+            arr[currentSize] = val;
+            currentSize++;
+        }
+        public void Append(T[] vals)
+        {
+            foreach (T v in vals)
+                Append(v);
+        }
+        public T Get(int ix)
+        {
+            if (ix >= currentSize)
+                throw new IndexOutOfRangeException("SLList.Get: index out of bounds");
+            return arr[ix];
+        }
+        public void Set(int ix, T val)
+        {
+            if (ix >= currentSize)
+                throw new IndexOutOfRangeException("SLList.Get: index out of bounds");
+            arr[ix] = val;
+        }
+        public void InsertAt(int ix, T val)
+        {
+            for (int i = currentSize; i > ix; i--)
+            {
+                arr[i] = arr[i - 1];
+            }
+            arr[ix] = val;
+            currentSize++;
+        }
+        public void DeleteAt(int ix)
+        {
+            for (int i = ix; i < currentSize - 1; i++)
+            {
+                arr[i] = arr[i + 1];
+            }
+            currentSize--;
+        }
+        public int Length()
+        {
+            return currentSize;
+        }
+        public void Print()
+        {
+            for (int i = 0; i < currentSize; i++)
+            {
+                Console.Write($"{i}:{arr[i]} ");
+            }
+            Console.WriteLine();
+        }
+    }
+    // Singly Linked List
+    public class SLList<T>
+    {
+        private T value;
+        private SLList<T> next;
+        int currentSize;
+        public SLList(int maxSize) // Isn't used!
+        {
+            value = default(T);
+            currentSize = 0;
+        }
+        public void Append(T val)
+        {
+            SLList<T> link = this;
+            while (link.next != null)
+                link = link.next;
+            link.next = new SLList<T>(1);
+            link._set(0, val);
+            currentSize++;
+        }
+        public void Append(T[] vals)
+        {
+            foreach (T v in vals)
+                Append(v);
+        }
+        public T Get(int ix)
+        {
+            return _get(ix);
+        }
+        private T _get(int ix)
+        {
+            SLList<T> link = this;
+            for (int i = 0; i < ix && link.next != null; i++)
+                link = link.next;
+            return link.value;
+        }
+        public void Set(int ix, T val)
+        {
+            _set(ix, val);
+        }
+        private void _set(int ix, T val)
+        {
+            SLList<T> link = this;
+            for (int i = 0; i < ix && link.next != null; i++)
+                link = link.next;
+            link.value = val;
+        }
+        public void InsertAt(int ix, T val)
+        {
+            SLList<T> link = this;
+            for (int i = 1; i < ix && link.next != null; i++)
+                link = link.next;
+            SLList<T> newLink = new SLList<T>(1);
+            newLink.value = val;
+            newLink.next = link.next;
+            link.next = newLink;
+            currentSize++;
+        }
+        public void DeleteAt(int ix)
+        {
+            SLList<T> link = this;
+            for (int i = 1; i < ix && link.next != null; i++)
+                link = link.next;
+            link.next = link.next.next;
+            currentSize++;
+        }
+        public int Length()
+        {
+            return currentSize;
+        }
+        public void Print()
+        {
+            SLList<T> link = this;
+            for (int i = 0; link.next != null; i++, link = link.next)
+            {
+                    Console.Write($"{i}:{link.value} ");
+            }
+            Console.WriteLine();
+        }
+    }
+}
